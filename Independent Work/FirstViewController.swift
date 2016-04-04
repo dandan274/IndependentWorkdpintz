cc391b10d98fa516502e4b21396b7c91a389405e
DANIELLE PINTZ <DANDAN274@GMAIL.COM>
2016-04-03 23:03:09 -0400

    Initial Commit

---
+++ Independent Work/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Independent Work
+//
+//  Created by Danielle Pintz on 4/3/16.
+//  Copyright © 2016 Danielle Pintz. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Independent WorkTests/Independent_WorkTests.swift
@@ -0,0 +1,36 @@
+//
+//  Independent_WorkTests.swift
+//  Independent WorkTests
+//
+//  Created by Danielle Pintz on 4/3/16.
+//  Copyright © 2016 Danielle Pintz. All rights reserved.
+//
+
+import XCTest
+@testable import Independent_Work
+
+class Independent_WorkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
